39e06581b25a0737ba45721c860595a21d936f06
Mathias Arlaud <mathias.arlaud@gmail.com>
2025-02-24 15:04:23 +0100

    feat(openapi): use `TypeInfo`'s `Type`

--- src/OpenApi/Factory/TypeFactoryTrait.php
+++ src/OpenApi/Factory/TypeFactoryTrait.php
@@ -14,7 +14,11 @@ declare(strict_types=1);
 namespace ApiPlatform\OpenApi\Factory;
 
 use Ramsey\Uuid\UuidInterface;
-use Symfony\Component\PropertyInfo\Type;
+use Symfony\Component\TypeInfo\Type;
+use Symfony\Component\TypeInfo\TypeIdentifier;
+use Symfony\Component\TypeInfo\Type\BackedEnumType;
+use Symfony\Component\TypeInfo\Type\CollectionType;
+use Symfony\Component\TypeInfo\Type\WrappingTypeInterface;
 use Symfony\Component\Uid\Ulid;
 use Symfony\Component\Uid\Uuid;
 
@@ -25,87 +29,71 @@ trait TypeFactoryTrait
 {
     private function getType(Type $type): array
     {
-        if ($type->isCollection()) {
-            $keyType = $type->getCollectionKeyTypes()[0] ?? null;
-            $subType = ($type->getCollectionValueTypes()[0] ?? null) ?? new Type($type->getBuiltinType(), false, $type->getClassName(), false);
+        if (!$type instanceof CollectionType) {
+            return $this->addNullabilityToTypeDefinition($this->makeBasicType($type), $type);
+        }
 
-            if (null !== $keyType && Type::BUILTIN_TYPE_STRING === $keyType->getBuiltinType()) {
-                return $this->addNullabilityToTypeDefinition([
-                    'type' => 'object',
-                    'additionalProperties' => $this->getType($subType),
-                ], $type);
-            }
-
+        if (!$type->getCollectionKeyType()->isIdentifiedBy(TypeIdentifier::STRING)) {
             return $this->addNullabilityToTypeDefinition([
                 'type' => 'array',
-                'items' => $this->getType($subType),
+                'items' => $this->getType($type->getCollectionValueType()),
             ], $type);
         }
 
-        return $this->addNullabilityToTypeDefinition($this->makeBasicType($type), $type);
+        return $this->addNullabilityToTypeDefinition([
+            'type' => 'object',
+            'additionalProperties' => $this->getType($type->getCollectionValueType()),
+        ], $type);
     }
 
     private function makeBasicType(Type $type): array
     {
-        return match ($type->getBuiltinType()) {
-            Type::BUILTIN_TYPE_INT => ['type' => 'integer'],
-            Type::BUILTIN_TYPE_FLOAT => ['type' => 'number'],
-            Type::BUILTIN_TYPE_BOOL => ['type' => 'boolean'],
-            Type::BUILTIN_TYPE_OBJECT => $this->getClassType($type->getClassName(), $type->isNullable()),
-            default => ['type' => 'string'],
-        };
-    }
-
-    /**
-     * Gets the JSON Schema document which specifies the data type corresponding to the given PHP class, and recursively adds needed new schema to the current schema if provided.
-     */
-    private function getClassType(?string $className, bool $nullable): array
-    {
-        if (null === $className) {
-            return ['type' => 'string'];
+        if ($type->isIdentifiedBy(TypeIdentifier::INT)) {
+            return ['type' => 'integer'];
         }
 
-        if (is_a($className, \DateTimeInterface::class, true)) {
-            return [
-                'type' => 'string',
-                'format' => 'date-time',
-            ];
+        if ($type->isIdentifiedBy(TypeIdentifier::FLOAT)) {
+            return ['type' => 'number'];
         }
-        if (is_a($className, \DateInterval::class, true)) {
-            return [
-                'type' => 'string',
-                'format' => 'duration',
-            ];
-        }
-        if (is_a($className, UuidInterface::class, true) || is_a($className, Uuid::class, true)) {
-            return [
-                'type' => 'string',
-                'format' => 'uuid',
-            ];
-        }
-        if (is_a($className, Ulid::class, true)) {
-            return [
-                'type' => 'string',
-                'format' => 'ulid',
-            ];
-        }
-        if (is_a($className, \SplFileInfo::class, true)) {
-            return [
-                'type' => 'string',
-                'format' => 'binary',
-            ];
-        }
-        if (is_a($className, \BackedEnum::class, true)) {
-            $enumCases = array_map(static fn (\BackedEnum $enum): string|int => $enum->value, $className::cases());
 
-            $type = \is_string($enumCases[0] ?? '') ? 'string' : 'integer';
+        if ($type->isIdentifiedBy(TypeIdentifier::BOOL)) {
+            return ['type' => 'boolean'];
+        }
 
+        if ($type->isIdentifiedBy(\DateTimeInterface::class)) {
+            return ['type' => 'string', 'format' => 'date-time'];
+        }
+
+        if ($type->isIdentifiedBy(\DateInterval::class)) {
+            return ['type' => 'string', 'format' => 'duration'];
+        }
+
+        if ($type->isIdentifiedBy(UuidInterface::class, Uuid::class)) {
+            return ['type' => 'string', 'format' => 'uuid'];
+        }
+
+        if ($type->isIdentifiedBy(Ulid::class)) {
+            return ['type' => 'string', 'format' => 'ulid'];
+        }
+
+        if ($type->isIdentifiedBy(\SplFileInfo::class)) {
+            return ['type' => 'string', 'format' => 'binary'];
+        }
+
+        $nullable = $type->isNullable();
+
+        while ($type instanceof WrappingTypeInterface) {
+            $type = $type->getWrappedType();
+        }
+
+        if ($type instanceof BackedEnumType) {
+            $enumCases = array_column($type->getClassName()::cases(), 'value');
             if ($nullable) {
                 $enumCases[] = null;
             }
 
             return [
-                'type' => $type,
+                'type' => $type->getBackingType()->isIdentifiedBy(TypeIdentifier::INT) ? 'integer' : 'string',
                 'enum' => $enumCases,
             ];
         }
@@ -120,7 +108,7 @@ trait TypeFactoryTrait
      */
     private function addNullabilityToTypeDefinition(array $jsonSchema, Type $type): array
     {
-        if (!$type->isNullable()) {
+        if (!$type->isNullable() || $type->isIdentifiedBy(TypeIdentifier::MIXED)) {
             return $jsonSchema;
         }
 
